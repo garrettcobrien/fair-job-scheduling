81b0017b259f8a9ef376bcc25658373adbbc58b8
garrettcobrien <gcobrien@purdue.edu>
2024-11-18 16:24:26 -0500

    edit

--- rando.py
+++ rando.py
@@ -5,6 +5,8 @@ from scipy.optimize import linear_sum_assignment
 bundles = [0,0]
 preferDif = [0,0]
 difAllocs = 0
+preferSam = [0,0]
+firstGetsPrefItem = 0
     
 rounds = 1000000
 for i in range(rounds):
@@ -21,11 +23,17 @@ for i in range(rounds):
         else:
             secondAlloc = valuations[match[0]][match[1]]
 
-    if firstAlloc == max(valuations[:, 0]):
+    #both agents get pref item
+    if firstAlloc == max(valuations[:, 0]) and secondAlloc == max(valuations[:, 1]):
         preferDif[0] += firstAlloc
-        difAllocs += 1
-    if secondAlloc == max(valuations[:, 1]):
         preferDif[1] += secondAlloc
+        difAllocs += 1
+    else:
+        #agent 1 gets pref item and agent 2 doesn't
+        if firstAlloc == max(valuations[:, 0]):
+            firstGetsPrefItem += 1
+        preferSam[0] += firstAlloc
+        preferSam[1] += secondAlloc
 
 
 
@@ -38,3 +46,10 @@ for i in range(rounds):
 print(bundles[0] / rounds, " ", bundles[1] / rounds)
 
 print(preferDif[0] / difAllocs, " ", preferDif[1] / difAllocs)
+print("num of rounds where alloc is different", difAllocs / rounds)
+
+print('num rounds where alloc is same, agent 1 gets preferred item', firstGetsPrefItem / rounds)
+
+sameAllocs = rounds - difAllocs
+
+print(preferSam[0] / sameAllocs, " ", preferSam[1] / sameAllocs)
